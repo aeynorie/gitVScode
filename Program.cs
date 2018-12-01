da6ae9f4058a767bae0b852347f6e3c2c5d429d4
Aohashx <ayanoriohashi@aohashx.local>
2018-12-02 02:45:02 +0900

    program.csの修正

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetData_FromFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadFiles rf = new ReadFiles("/Users/ayanoriohashi/Data/Repos/File");
+        }
+    }
+}
