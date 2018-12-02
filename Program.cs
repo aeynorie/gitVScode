0c4426afb336fac60f6862c5c6fa6b9caf04c719
Aohashx <ayanoriohashi@aohashx.local>
2018-12-02 12:29:33 +0900

    program.csの修正

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace GetData_FromFiles
 {
@@ -7,6 +10,22 @@ namespace GetData_FromFiles
         static void Main(string[] args)
         {
             ReadFiles rf = new ReadFiles("/Users/ayanoriohashi/Data/Repos/File");
+        }
+    }
+    
+    public class ReadFiles
+    {
+        private string filePath;
+        public ReadFiles(string dir){
+            this.filePath = dir;
+            string[] files = Directory.GetFiles(dir, "*.txt");
+            
+            for(var i = 0; i<files.Length; i++){
+               StreamReader sr = new StreamReader(files[i]);
+                string text = sr.ReadToEnd();
+                sr.Close();
+                Console.WriteLine(text);
+            }   
         }
     }
 }
